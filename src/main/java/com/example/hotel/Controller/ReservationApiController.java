62557ee3de0a5dbcf8b6d8663d28eab40c4fd139
kikiga1212 <kikiga1212@gmail.com>
2026-04-13 15:05:16 +0900

    ReservationApiController

---
+++ src/main/java/com/example/hotel/Controller/ReservationApiController.java
@@ -0,0 +1,50 @@
+package com.example.hotel.Controller;
+
+import com.example.hotel.DTO.ReservationDTO;
+import com.example.hotel.Repository.ReservationRepository;
+import com.example.hotel.Service.ReservationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// 예약 테이블에 대한 비동기식 요청처리
+@RestController
+@RequestMapping("/api/reservations")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
+public class ReservationApiController {
+    private final ReservationRepository reservationRepository;
+    private final ReservationService reservationService;
+
+    // 예약 생성
+    @PostMapping
+    public ResponseEntity<?> createReservation(@Valid @RequestBody ReservationDTO dto){
+        try{
+            ReservationDTO created = reservationService.createReservation(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(created); // 201 반환
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage()); // 400오류
+        }
+    }
+
+    // 내 예약 조회
+    @GetMapping("/my")
+    public ResponseEntity<List<ReservationDTO>> getMyReservation(@RequestParam String email){
+        return ResponseEntity.ok(reservationService.getResevationByEmail(email));
+    }
+
+    // 예약 취소
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<?> cancelReservation(@PathVariable Long id){
+        try{
+            reservationService.cancelReservation(id);
+            return ResponseEntity.ok("예약이 취소되었습니다.");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
